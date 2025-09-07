2bc1a0c532e496f02cc925a9119f6888b766f00c
mtfedev <fedorowiczmati@outlook.com>
2025-09-07 02:23:52 +0200

    add_handlers

--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.24.3
 
 require (
 	github.com/andybalholm/brotli v1.1.0 // indirect
-	github.com/gofiber/fiber/v2 v2.52.9 // indirect
+	github.com/gofiber/fiber/v2 v2.52.9
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/klauspost/compress v1.17.9 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
--- main.go
+++ main.go
@@ -1,9 +1,27 @@
 package main
 
-inport (
+import (
+	"flag"
+
 	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
-	app ;= fiber.new()
+	listenAddr := flag.String("litebAddr", ":5000", "The listen adders of the API server")
+	flag.Parse()
+	app := fiber.New()
+	apiv1 := app.Group("/api/v1")
+
+	app.Get("/foo", handleFoo)
+
+	apiv1.Get("/user", handleUser)
+	app.Listen(*listenAddr)
+}
+
+func handleFoo(c *fiber.Ctx) error {
+	return c.JSON(map[string]string{"msg": "OK"})
+}
+
+func handleUser(c *fiber.Ctx) error {
+	return c.JSON(map[string]string{"msg": "Bob Foo"})
 }
